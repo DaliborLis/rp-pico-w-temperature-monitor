0d0205ea9d64b7debdc03950a8b3eb89e423483f
Dalibor Lis <dalibor.lis@gmail.com>
2024-01-15 16:16:10 +0100

    Improved data sending

--- main.py
+++ main.py
@@ -23,6 +23,35 @@ def connect_to_server():
     s.connect(addr)
     return s
 
+def send_data_over_socket(s, data):
+    data_length = len(request)
+    total_sent = 0
+    while total_sent < data_length:
+        sent = s.send(data[total_sent:])
+        if sent == 0:
+            raise Exception("sending data: socket connection broken")
+        total_sent = total_sent + sent
+
+def receive_data_over_socket(s, data_length):
+    chunks = []
+    bytes_received = 0
+    while bytes_received < data_length:
+        chunk = s.recv(min(data_length - bytes_received, 2048))
+        if chunk == b'':
+            raise Exception("receiving data: socket connection broken")
+        chunks.append(chunk)
+        bytes_received = bytes_received + len(chunk)
+    return b''.join(chunks)
+
+def receive_all(s, buffer_size=4096):
+    data = b""
+    while True:
+        chunk = s.recv(buffer_size)
+        if not chunk:
+            break
+        data += chunk
+    return data
+
 do_connect()
 
 # DHT22 libray is available at
@@ -36,13 +65,19 @@ while True:
     else:
         print("{}'C  {}%".format(T,H))
         data = '{{"temperature": "{}", "humidity": "{}", "room": "obyvak"}}'.format(T,H)
+        print(len(data))
+        s = None
         try:
             s = connect_to_server()
-            request = 'POST /tempMonitor/reportTH HTTP/1.1\r\nHost: {}:{}'.format(SERVER_IP, SERVER_PORT) + '\r\nContent-Type:application/json\r\nContent-Length:' + str(len(data)) + '\r\n\r\n' + data + '\r\n\r\n'
+            request = 'POST /tempMonitor/reportTH HTTP/1.1\r\nHost: {}:{} \r\nAccept: */*\r\nContent-Type: application/json\r\nContent-Length: {}\r\n\r\n{}'.format(SERVER_IP, SERVER_PORT, len(data), data).encode('UTF-8')
             print(request)
-            sent = s.send(request)
-            # print(sent)
+            #s.sendall(request)
+            send_data_over_socket(s, request)
+            #print(receive_data_over_socket(s, 160))
+            #print(receive_all(s))
         except OSError as e:
+            print(e)
+        except Exception as e:
             print(e)
         finally:
             if s != None:
